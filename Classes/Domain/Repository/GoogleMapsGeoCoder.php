4450ae96d275056b2a16d8450aa39bc987f67a00
Bernhard Schmitt <b.schmitt@core4.de>
2017-09-29 11:38:07 +0200

    Task: Adjust to new Geographic Library API

--- Classes/Domain/Repository/GoogleMapsGeoCoder.php
+++ Classes/Domain/Repository/GoogleMapsGeoCoder.php
@@ -5,7 +5,8 @@ namespace Nezaniel\GeographicLibrary\GoogleMapsAdapter\Domain\Repository;
 /*                                                                                                 *
  * This script belongs to the Neos Flow package "Nezaniel.GeographicLibrary.GoogleMapsAdapter".   *
  *                                                                                                 */
-use Nezaniel\GeographicLibrary\Application\Value\Coordinates;
+use Nezaniel\GeographicLibrary\Application\Value\CountryCode;
+use Nezaniel\GeographicLibrary\Application\Value\GeoCoordinates;
 use Nezaniel\GeographicLibrary\Domain\Exception\NoSuchCoordinatesException;
 use Nezaniel\GeographicLibrary\Domain\Repository\GeoCoderInterface;
 use Neos\Flow\Annotations as Flow;
@@ -25,7 +26,7 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
     /**
      * {@inheritdoc}
      */
-    public function fetchCoordinatesByAddress(string $address): Coordinates
+    public function fetchCoordinatesByAddress(string $address): GeoCoordinates
     {
         $request = curl_init('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false' . ($this->apiKey ? '&key=' . $this->apiKey : ''));
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
@@ -41,7 +42,7 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
     /**
      * {@inheritdoc}
      */
-    public function fetchCoordinatesByPostalCode(string $zip, string $countryCode): Coordinates
+    public function fetchCoordinatesByPostalCode(string $zip, string $countryCode): GeoCoordinates
     {
         $request = curl_init('https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:' . $zip . '|country:' . $countryCode . '&sensor=false' . ($this->apiKey ? '&key=' . $this->apiKey : ''));
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
@@ -56,10 +57,10 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
 
     /**
      * @param \stdClass $response
-     * @return Coordinates
+     * @return GeoCoordinates
      * @throws NoSuchCoordinatesException
      */
-    protected function getCoordinatesFromResponse(\stdClass $response): Coordinates
+    protected function getCoordinatesFromResponse(\stdClass $response): GeoCoordinates
     {
         if (empty($response->results)) {
             throw new NoSuchCoordinatesException();
