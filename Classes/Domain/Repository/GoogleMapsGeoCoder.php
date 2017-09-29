72b4684ffba0fefdda29a945ca54eeb16ae9f9e4
Bernhard Schmitt <b.schmitt@core4.de>
2017-09-29 11:39:04 +0200

    Feature: Take current language into account and provide more data to result GeoCoordinates

--- Classes/Domain/Repository/GoogleMapsGeoCoder.php
+++ Classes/Domain/Repository/GoogleMapsGeoCoder.php
@@ -10,12 +10,19 @@ use Nezaniel\GeographicLibrary\Application\Value\GeoCoordinates;
 use Nezaniel\GeographicLibrary\Domain\Exception\NoSuchCoordinatesException;
 use Nezaniel\GeographicLibrary\Domain\Repository\GeoCoderInterface;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\I18n;
 
 /**
  * @Flow\Scope("singleton")
  */
 class GoogleMapsGeoCoder implements GeoCoderInterface
 {
+    /**
+     * @Flow\Inject
+     * @var I18n\Service
+     */
+    protected $localizationService;
+
     /**
      * @Flow\InjectConfiguration(path="api.key")
      * @var string
@@ -28,7 +35,14 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
      */
     public function fetchCoordinatesByAddress(string $address): GeoCoordinates
     {
-        $request = curl_init('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false' . ($this->apiKey ? '&key=' . $this->apiKey : ''));
+        $requestUri = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false';
+        if ($this->apiKey) {
+            $requestUri .= '&key=' . $this->apiKey;
+        }
+        if ($this->localizationService->getConfiguration()->getCurrentLocale()->getLanguage()) {
+            $requestUri .= '&language=' . $this->localizationService->getConfiguration()->getCurrentLocale()->getLanguage();
+        }
+        $request = curl_init($requestUri);
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
         $response = json_decode(curl_exec($request));
 
@@ -44,7 +58,14 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
      */
     public function fetchCoordinatesByPostalCode(string $zip, string $countryCode): GeoCoordinates
     {
-        $request = curl_init('https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:' . $zip . '|country:' . $countryCode . '&sensor=false' . ($this->apiKey ? '&key=' . $this->apiKey : ''));
+        $requestUri = 'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:' . $zip . '|country:' . $countryCode . '|locale:de&sensor=false';
+        if ($this->apiKey) {
+            $requestUri .= '&key=' . $this->apiKey;
+        }
+        if ($this->localizationService->getConfiguration()->getCurrentLocale()->getLanguage()) {
+            $requestUri .= '&language=' . $this->localizationService->getConfiguration()->getCurrentLocale()->getLanguage();
+        }
+        $request = curl_init($requestUri);
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
         $response = json_decode(curl_exec($request));
 
@@ -65,9 +86,36 @@ class GoogleMapsGeoCoder implements GeoCoderInterface
         if (empty($response->results)) {
             throw new NoSuchCoordinatesException();
         } else {
-            $location = $response->results[0]->geometry->location;
+            $primaryLocation = $response->results[0];
+            $coordinates = $primaryLocation->geometry->location;
+            $address = null;
+            $addressLocality = null;
+            $postalCode = null;
+            $countryCode = null;
 
-            return new Coordinates($location->lat, $location->lng);
+            foreach ($primaryLocation->address_components as $addressComponent) {
+                switch (reset($addressComponent->types)) {
+                    case 'country':
+                        $countryCode = new CountryCode($addressComponent->short_name);
+                        break;
+                    case 'locality':
+                        $addressLocality = $addressComponent->short_name;
+                        break;
+                    case 'postal_code':
+                        $postalCode = $addressComponent->short_name;
+                        break;
+                    default:
+                }
+            }
+
+            return new GeoCoordinates(
+                $coordinates->lat,
+                $coordinates->lng,
+                null,
+                $postalCode . ' ' . $addressLocality,
+                $postalCode,
+                $countryCode
+            );
         }
     }
 }
